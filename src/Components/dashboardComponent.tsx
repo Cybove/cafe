1b34e42c268c3eeaa726b2448816ede641096727
Cybove <ataugurcuklu@gmail.com>
2024-10-16 14:43:14 +0300

    dashboard update

--- src/Components/dashboardComponent.tsx
+++ src/Components/dashboardComponent.tsx
@@ -5,8 +5,26 @@ export const Dashboard = () => {
 
     <div>
       <div id="orders-container">
-        <div hx-ext="ws" ws-connect="/ws"></div>
-        <div hx-get="/api/orders" hx-swap="innerHTML" hx-trigger=" update from:window"></div>
+        <div class="container mx-auto">
+          <div class="rounded-lg border bg-white shadow-sm w-full mb-6">
+            <div class="flex flex-col space-y-1.5 p-6 bg-gradient-to-r from-purple-500 to-pink-500 text-white">
+              <h3 class="text-center whitespace-nowrap tracking-tight text-4xl font-bold">Dashboard</h3>
+              <p class="text-center text-md text-gray-100">Manage Orders</p>
+            </div>
+            <div class="p-6 pt-6">
+              <div class="w-full md:w-auto">
+                <div class="h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground grid w-full grid-cols-3">
+                  {/* Tabs */}
+                </div>
+              </div>
+              <div>
+                <div hx-ext="ws" ws-connect="/ws"></div>
+                <div hx-get="/api/orders" hx-swap="innerHTML" hx-trigger=" update from:window"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
       <script>
         {`
--- src/Routes/API/orders.tsx
+++ src/Routes/API/orders.tsx
@@ -9,22 +9,27 @@ export const orders = (app: Elysia) => {
         const orders = getOrders();
         const orderItems = getAllOrderItems();
         return (
-            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-4">
+            <div class="flex flex-wrap justify-start gap-5 p-4">
                 {orders.map((order) => (
-                    <div class="rounded-lg border bg-white text-gray-800 shadow-sm overflow-hidden transition-all duration-300 hover:shadow-lg">
-                        <div class="flex flex-col space-y-2 p-6 bg-gray-800 text-white">
-                            <div class="whitespace-nowrap text-3xl font-bold leading-none tracking-tight flex justify-between items-center text-center">
-                                {order.customer_token}
-                            </div>
-                        </div>
-
-                        <div class="p-6 pt-4">
-                            <div class="space-y-2">
-                                {orderItems.filter(item => item.customer_token === order.customer_token).map((orderItem) => (<div class="flex justify-between items-center p-3 bg-gray-100 rounded shadow hover:bg-gray-300 transition">
-                                    <span class="font-semibold text-gray-800">{orderItem.name}</span>
-                                    <span class="text-gray-700">{orderItem.price} TL</span>
+                    <div class="flex-grow-0 flex-shrink-0 basis-[calc(25%-1.25rem)] min-w-[250px]">
+                        <div class="rounded-lg border bg-white text-gray-800 shadow-sm overflow-hidden transition-all duration-300 hover:shadow-lg">
+                            <div class="flex flex-col space-y-2 p-4 bg-gray-800 text-white">
+                                <div class="whitespace-nowrap text-5xl font-bold leading-none tracking-tight flex justify-center items-center text-center">
+                                    {order.customer_token}
                                 </div>
-                                ))}
+                            </div>
+                            <div class="p-4 pt-4">
+                                <div class="space-y-2">
+                                    {orderItems.filter(item => item.customer_token === order.customer_token).map((orderItem) => (<div class="flex justify-between items-center p-3 bg-gray-100 rounded shadow hover:bg-gray-300 transition">
+                                        <span class="font-semibold text-gray-800">{orderItem.name}</span>
+                                        <span class="text-gray-700">{orderItem.price} TL</span>
+                                    </div>
+                                    ))}
+                                    <div class="text-center">
+                                        <span class="font-semibold text-gray-800">Total:</span>
+                                        <span class="text-gray-700 font-semibold">{orderItems.filter(item => item.customer_token === order.customer_token).reduce((total, item) => total + item.price, 0)} TL</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
